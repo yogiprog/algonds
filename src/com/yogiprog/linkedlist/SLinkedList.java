fd5773c72372dbdfcece4cfe4333ea5953a87963
yogiprog <autodidact365@gmail.com>
2020-04-25 14:16:10 -0700

    add-SLL init and add node operations

--- src/com/yogiprog/linkedlist/SLinkedList.java
+++ src/com/yogiprog/linkedlist/SLinkedList.java
@@ -1,7 +1,66 @@
 package com.yogiprog.linkedlist;
 
 public class SLinkedList {
-	public static void main(String[] args) {
+
+	public SNode head;
+
+	SLinkedList() {
+		this.head = null;
+	}
+
+	SLinkedList(int data) {
+		this.head = new SNode(data);
+	}
+
+	// ADD operations - add(int data) (default), addLast(int data),
+	// addAtPostion(int postion, int data)
+
+	void add(int data) {
+		SNode itr = this.head;
+		SNode node = new SNode(data);
+		if (itr != null) {
+			while(itr.getNext()!=null)
+				itr=itr.getNext();
+			itr.setNext(node);	
+		}
+		else 
+			this.head= node;
+	}
+
+	void addLast(int data) {
+	
+		SNode itr = this.head;
+		SNode node = new SNode(data);
+		while(itr.getNext()!=null)
+			itr=itr.getNext();
+		itr.setNext(node);	
 		
+	}
+	
+	
+	void addAtPostion(int data, int position) {
+		
+		
+	}
+	
+	
+	
+	void display() {
+		SNode itr = head;
+		if (itr == null)
+			System.out.println("Linked list is empty!");
+		while (itr != null) {
+			System.out.println(itr.getData());
+			itr = itr.getNext();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		SLinkedList SLL = new SLinkedList();
+		SLL.add(1);
+		SLL.add(2);
+		SLL.addLast(3);
+		SLL.display();
 	}
 }
--- src/com/yogiprog/linkedlist/SNode.java
+++ src/com/yogiprog/linkedlist/SNode.java
@@ -2,4 +2,29 @@ package com.yogiprog.linkedlist;
 
 public class SNode {
 
+	private int data;
+	private SNode next;
+
+	public SNode(int data) {
+		super();
+		this.data = data;
+		this.next = null;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public SNode getNext() {
+		return next;
+	}
+
+	public void setNext(SNode next) {
+		this.next = next;
+	}
+
 }
