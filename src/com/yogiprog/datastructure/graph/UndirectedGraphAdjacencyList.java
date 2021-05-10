ef2f3d9acf46f6821c80f2fa02f48567ef458d65
Yogesh Mhase <yogeshmhase89@gmail.com>
2021-05-09 22:36:54 -0700

    add graph

--- src/com/yogiprog/datastructure/graph/UndirectedGraphAdjacencyList.java
+++ src/com/yogiprog/datastructure/graph/UndirectedGraphAdjacencyList.java
@@ -1,9 +1,34 @@
 package com.yogiprog.datastructure.graph;
 
+import java.util.ArrayList;
+
 public class UndirectedGraphAdjacencyList {
-
-  public static void main(String[] args) {
-
+  private ArrayList<ArrayList<Node>> graph ;
+  UndirectedGraphAdjacencyList(){
+    this.graph = new ArrayList<ArrayList<Node>>();
+  }
+  public void addNode(int data, int from){
+      ArrayList<Node> temp = graph.get(from);
+      temp.add(new Node(data));
   }
 
+  public void display(){
+    System.out.println(">>>>>>>>>>>>>>>>>>>");
+    for(ArrayList<Node> list : graph){
+      for (Node n : list){
+        System.out.println(n.data);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    UndirectedGraphAdjacencyList g = new UndirectedGraphAdjacencyList();
+    System.out.println(">>>>>>>>>>>>>>>>>>>");
+    g.addNode(1,2);
+    g.display();
+  }
+
+
 }
+
+
